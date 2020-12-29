7736ec2e58ef09f155804cae8faf53440defd6dd
Sunghoon Ahn <ahnsunghoon0215@gmail.com>
2020-12-28 23:33:18 -0500

    used props to pass data from one component to another

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 
-// make component called Subject
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-            <h1>WEB</h1>
-            world wide web!
-      </header>   
-    );
-  }
-}
 
 class TOC extends Component {
   render() {
@@ -31,9 +20,20 @@ class Content extends Component {
   render() {
     return (
       <article>
-        <h2>HTML</h2>
-        HTML is HyperText Markup Language.
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
       </article>
+    );
+  }
+}
+
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+            <h1>{this.props.title}</h1>
+            {this.props.sub}
+      </header>   
     );
   }
 }
@@ -42,9 +42,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Subject></Subject>
+        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject title="React" sub="For UI"></Subject>
         <TOC></TOC>
-        <Content></Content>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
   }
